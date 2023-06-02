fa8142bec736667db4dda15b339858c1f4a6f188
jinkai <jinkai@bigo.sg>
2023-06-02 16:52:42 +0800

    exp09

--- train.sh
+++ train.sh
@@ -27,15 +27,27 @@
 #   /data1/jk/imagenet/ILSVRC/Data/CLS-LOC/
 
 
+# python main_moco.py \
+#   -a extra.mobilenet_v2 \
+#   --use-yuv \
+#   --use-autoaug \
+#   --epochs 800 \
+#   --input-size 224 \
+#   --schedule 480 640 \
+#   --lr 0.03 \
+#   --batch-size 256 \
+#   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
+#   --mlp --moco-t 0.2 --aug-plus --cos \
+#   /data/jk/imagenet/ILSVRC/Data/CLS-LOC/ > Exp08.moco2.mobilenet_v2.yuv709f.auotaug.e800.log 2>&1
+
 python main_moco.py \
   -a extra.mobilenet_v2_050 \
   --use-yuv \
-  --use-autoaug \
   --epochs 800 \
-  --input-size 224
+  --input-size 384 \
   --schedule 480 640 \
-  --lr 0.0125 \
-  --batch-size 128 \
+  --lr 0.03 \
+  --batch-size 256 \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   --mlp --moco-t 0.2 --aug-plus --cos \
-  /data1/jk/imagenet/ILSVRC/Data/CLS-LOC/ > extra.mobilenet_v2_050.log 2>&1
+  /data/jk/imagenet/ILSVRC/Data/CLS-LOC/ > Exp09.moco2.mobilenet_v2_050.yuv709f.i384.e800.log 2>&1
